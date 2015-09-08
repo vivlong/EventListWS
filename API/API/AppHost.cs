9c94fa3522e9c0fd32a2460f98d44a6c0a197a7a
vivlong <vivlonglz@gmail.com>
2015-09-08 11:39:31 +0800

    1. Add Templates/IndexOperations.html

--- API/API/AppHost.cs
+++ API/API/AppHost.cs
@@ -35,7 +35,7 @@ namespace TmsWS
                 //  { "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" },
                 //  { "Access-Control-Allow-Headers", "Content-Type, Signature" },
                 //},
-                EnableFeatures = Feature.All.Remove(Feature.Xml | Feature.Jsv | Feature.Csv | Feature.Soap11 | Feature.Soap12)
+                EnableFeatures = Feature.All.Remove(Feature.Xml | Feature.Jsv | Feature.Csv | Feature.Soap11 | Feature.Soap12 | Feature.Soap),
             });
             CorsFeature cf = new CorsFeature(allowedOrigins: "*", allowedMethods: "GET, POST, PUT, DELETE, OPTIONS", allowedHeaders: "Content-Type, Signature", allowCredentials: false);
             this.Plugins.Add(cf);
